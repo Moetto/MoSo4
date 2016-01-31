9ee7a7e20623c7f7b2073c5921e3d346114c7252
Antti Möttönen <antti.moettoenen@gmail.com>
2016-01-31 22:07:44 +0200

    Add facebook login

--- app/src/main/java/moetto/moso4/MainActivity.java
+++ app/src/main/java/moetto/moso4/MainActivity.java
@@ -1,13 +1,63 @@
 package moetto.moso4;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MoSo4 MainActivity";
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_main);
+
+        try {
+            LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+
+            loginButton.setReadPermissions("user_posts");
+            loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    Toast.makeText(MainActivity.this, "Loggen in facebook", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onCancel() {
+                    Toast.makeText(MainActivity.this, "Facebook login cancelled. Application does not work without facebook login", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onError(FacebookException error) {
+                    Toast.makeText(MainActivity.this, "Facebook login failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (NullPointerException nux) {
+            Log.e(TAG, Log.getStackTraceString(nux));
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
