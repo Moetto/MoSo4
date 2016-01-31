6c8c8a96dec579d5c259effd5bae3af25165995f
Antti Möttönen <antti.moettoenen@gmail.com>
2016-01-31 15:29:16 +0200

    Basic receiver logging phone events

--- app/src/main/java/moetto/moso4/CallMonitor.java
+++ app/src/main/java/moetto/moso4/CallMonitor.java
@@ -1,26 +1,27 @@
 package moetto.moso4;
 
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
-import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 /**
  * Created by moetto on 1/29/16.
  */
-public class CallMonitor extends Service {
+public class CallMonitor extends BroadcastReceiver {
+    private final static String TAG = "CallMonitor";
 
-    
-
-    @Nullable
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "Reveived broadcast");
+
+        if (intent.hasExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)) {
+            Log.d(TAG, "Incoming call from number " + intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
+        }
+
+        if (intent.hasExtra(TelephonyManager.EXTRA_STATE)) {
+            Log.d(TAG, "Status: " + intent.getStringExtra(TelephonyManager.EXTRA_STATE));
+        }
     }
 }
-
-
