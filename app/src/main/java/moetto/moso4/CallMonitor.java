158ebfdb5288dd35f71a72642112f10f25e628d4
Antti Möttönen <antti.moettoenen@gmail.com>
2016-01-31 19:29:03 +0200

    Query name from contacts

--- app/src/main/java/moetto/moso4/CallMonitor.java
+++ app/src/main/java/moetto/moso4/CallMonitor.java
@@ -3,6 +3,8 @@ package moetto.moso4;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
@@ -14,14 +16,29 @@ public class CallMonitor extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, "Reveived broadcast");
+        Log.d(TAG, "Received broadcast");
 
-        if (intent.hasExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)) {
-            Log.d(TAG, "Incoming call from number " + intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
-        }
+        if (TelephonyManager.EXTRA_STATE_RINGING.equals(intent.getStringExtra(TelephonyManager.EXTRA_STATE))) {
+            Log.d(TAG, "Incoming call from " + intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
+            Cursor contacts = context.getContentResolver().query(
+                    ContactsContract.Contacts.CONTENT_URI,
+                    new String[]{ContactsContract.Contacts.DISPLAY_NAME},
+                    ContactsContract.Contacts.DISPLAY_NAME + " = ?",
+                    new String[]{intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)},
+                    null
+            );
+            if (contacts != null) {
+                while (contacts.moveToNext()) {
+                    Log.d(TAG, contacts.getString(0));
+                }
+            }
+            else {
+                Log.d(TAG, "Unknown number");
+                return;
+            }
+            //TODO get name from facebook
 
-        if (intent.hasExtra(TelephonyManager.EXTRA_STATE)) {
-            Log.d(TAG, "Status: " + intent.getStringExtra(TelephonyManager.EXTRA_STATE));
+            //TODO post notification
         }
     }
 }
