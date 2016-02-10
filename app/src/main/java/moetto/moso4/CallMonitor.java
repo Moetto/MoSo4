0fc2f3b4c48b24fe437dd55274b1821539d1b181
Antti Möttönen <antti.moettoenen@gmail.com>
2016-02-10 12:26:22 +0200

    Work on caller identification

--- app/src/main/java/moetto/moso4/CallMonitor.java
+++ app/src/main/java/moetto/moso4/CallMonitor.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.telephony.TelephonyManager;
 import android.util.Log;
@@ -14,6 +15,9 @@ import com.facebook.GraphRequestAsyncTask;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * Created by moetto on 1/29/16.
  */
@@ -23,19 +27,22 @@ public class CallMonitor extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "Received broadcast");
-
+        String name;
         if (TelephonyManager.EXTRA_STATE_RINGING.equals(intent.getStringExtra(TelephonyManager.EXTRA_STATE))) {
             Log.d(TAG, "Incoming call from " + intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER));
+            Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)));
             Cursor contacts = context.getContentResolver().query(
-                    ContactsContract.Contacts.CONTENT_URI,
-                    new String[]{ContactsContract.Contacts.DISPLAY_NAME},
-                    ContactsContract.Contacts.DISPLAY_NAME + " = ?",
-                    new String[]{intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)},
+                    uri,
+                    new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME},
+                    null,
+                    null,
                     null
             );
+
             if (contacts != null) {
-                while (contacts.moveToNext()) {
+                if (contacts.moveToFirst()) {
                     Log.d(TAG, contacts.getString(0));
+                    name = contacts.getString(0);
                 }
             } else {
                 Log.d(TAG, "Unknown number");
@@ -43,17 +50,25 @@ public class CallMonitor extends BroadcastReceiver {
             }
 
             //TODO get name from facebook
-            GraphRequestAsyncTask graphRequest = new GraphRequest(AccessToken.getCurrentAccessToken(),
+            GraphRequestAsyncTask friends = new GraphRequest(AccessToken.getCurrentAccessToken(),
                     "/me/friends",
                     null,
                     HttpMethod.GET,
                     new GraphRequest.Callback() {
                         @Override
                         public void onCompleted(GraphResponse response) {
-                            Log.d(TAG, "" + response.getRawResponse());
+                            Log.d(TAG, "Faboresponse: " + response.getRawResponse());
                             //TODO post notification
                         }
                     }).executeAsync();
+            /*
+            JSONArray friendList = friends.getJSONObject().getJSONArray("data");
+            for (JSONObject friend : friendList) {
+                if (friend.getString("name").equals())
+            }
+            */
+
+
         }
     }
 }
